d930ba08413de3035d5604342256b3c40eec774f
Kiraygmi <kiravonhell@gmail.com>
2023-03-19 23:02:49 +0200

    Initial commit

---
+++ im also confused/im also confused/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("miaumiau");
+
+
+
+int a;
+a =Int32.Parse(Console.ReadLine());
+
+int b = a % 2;
+
+
+
+
+
+if (b == 0) Console.WriteLine("Par");
+if (b == 1) Console.WriteLine("Impar");
