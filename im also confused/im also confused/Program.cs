d42e99883f0294e06517de59c6854b96849c81ec
Kiraygmi <kiravonhell@gmail.com>
2023-03-19 23:08:55 +0200

    removed empty lines and added commentary

--- im also confused/im also confused/Program.cs
+++ im also confused/im also confused/Program.cs
@@ -1,15 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("miaumiau");
 
-
+//i have no clue what im doing tbh
 
 int a;
 a =Int32.Parse(Console.ReadLine());
 
 int b = a % 2;
-
-
-
 
 
 if (b == 0) Console.WriteLine("Par");
