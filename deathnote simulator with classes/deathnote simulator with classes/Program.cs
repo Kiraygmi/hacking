229eee64635a0a5f72dfd64cfac2a2e833d228ea
Kiraygmi <kiravonhell@gmail.com>
2023-03-21 01:10:45 +0200

    problem with classes death note simulator

---
+++ deathnote simulator with classes/deathnote simulator with classes/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Shinigamipower Rem = new Shinigamipower();
+
+
+Rem.deathnote("Lind L. Tailor");
+Rem.deathnote("Takuo Shibuimaru", "hit by truck");
+Rem.deathnote("Naomi Misora", "suicide");
+Rem.deathnote("Naoki Koro Kitayama");
+
+
+class Shinigamipower
+{
+    public Shinigamipower()
+    {
+
+    }
+
+    public void deathnote(string name, string cause)
+    {
+        Console.WriteLine(name + " death caused by " + cause);
+    }
+    public void deathnote(string name)
+    {
+        Console.WriteLine(name + " death caused by heart attack");
+    }
+}
