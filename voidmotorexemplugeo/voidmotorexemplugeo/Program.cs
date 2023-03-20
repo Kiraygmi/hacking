6fe69b38488c5054c32d41ef7bd586a8f8bbfb5e
Kiraygmi <kiravonhell@gmail.com>
2023-03-21 01:10:18 +0200

    example of how a function works

---
+++ voidmotorexemplugeo/voidmotorexemplugeo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+combustionEngine("Z14", 90, 200, 120, 1.6f, "gasoline");
+combustionEngine("Irinuca", 10, 200, 120, 10f, "gasoline");
+
+
+void combustionEngine(string model, int horsePower, int torque, float weight, float volume, string fuelType)
+{
+    Console.WriteLine("motor cu modelul " + model + " are consum de " + horsePower * volume);
+}
+
+
+combustionEngine(/* model */ "Z14", /* horsePower */ 90, /* torque */ 200, /* weight */ 120, /* volume */ 1.6f, /* fuelType */ "gasoline");
+combustionEngine(/* model */ "Irinuca", /* horsePower */ 10, /* torque */ 200, /* weight */ 120, /* volume */ 10f, /* fuelType */ "gasoline");
+
+
