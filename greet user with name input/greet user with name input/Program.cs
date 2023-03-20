e1d114c756d6897caa2790d67a903498842afdee
Kiraygmi <kiravonhell@gmail.com>
2023-03-21 01:08:20 +0200

    problem that greets the user with given name

---
+++ greet user with name input/greet user with name input/Program.cs
@@ -0,0 +1,16 @@
+﻿//Write a program that asks the user for their name and greets them with their name.
+
+string name;
+name = Console.ReadLine();
+
+
+
+
+if (name == "Luna")
+{
+    Console.WriteLine("meowmeow");
+}
+else
+{
+    Console.WriteLine("Hello " + name + " How are you today?");
+}
