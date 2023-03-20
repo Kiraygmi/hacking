0642327775dab4318c01dfad5ad0ebd881b27764
Kiraygmi <kiravonhell@gmail.com>
2023-03-21 01:09:35 +0200

    console program that judges you based on your favourite game

---
+++ console judges you based on game/console judges you based on game/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+
+
+
+string game;
+Console.WriteLine("Hi, I'm here to judge you today.");
+Console.WriteLine("What game do you play the most in your free time?");
+game = Console.ReadLine();
+game = game.ToLower();
+
+
+if (game == "league of legends")
+{
+    Console.WriteLine("racist");
+}
+else if (game == "fortnite")
+{
+    Console.WriteLine("stay away from me");
+}
+else if (game == "genshin impact")
+{
+    Console.WriteLine("touch grass");
+}
+else if (game == "dead by daylight")
+{
+    Console.WriteLine("I'm sorry for you");
+}
+else if (game == "elden ring")
+{
+    Console.WriteLine("masochist");
+}
+else if (game == "minecraft")
+{
+    Console.WriteLine("get some sleep");
+}
+else if (game == "valorant")
+{
+    Console.WriteLine("stay away from me");
+}
+else if (game == "overwatch 2")
+{
+    Console.WriteLine("you miss ow 1 don't you");
+}
+else if (game == "hogwarts legacy")
+{
+    Console.WriteLine("millenial");
+}
+else if (game == "sims")
+{
+    Console.WriteLine("I love you");
+}
+else if (game == "pokemon")
+{
+    Console.WriteLine("marry me");
+}
+else
+{
+    Console.WriteLine("Sorry, my mom didn't tell me how to judge you on this game choice.");
+}
+
+Console.ReadLine();
