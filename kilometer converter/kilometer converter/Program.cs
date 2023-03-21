51ce8fe104e1c91e9521b73a13ae88277724bfd5
Kiraygmi <kiravonhell@gmail.com>
2023-03-21 20:21:45 +0200

    programs that convert miles to kilometers and vice versa

---
+++ kilometer converter/kilometer converter/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Write a program that asks the user for a value in miles as input, and then prints that value converted to kilometers
+
+
+double mile;
+Console.WriteLine("Miles to Kilometers converter");
+Console.WriteLine("Miles=");
+
+
+
+mile = Convert.ToDouble(Console.ReadLine());
+
+
+double kilometer;
+kilometer = mile * 1.60934;
+Console.WriteLine("Kilometers= " + kilometer);
+
+double kilometer2;
+Console.WriteLine("Kilometers to miles converter");
+Console.WriteLine("Kilometers=");
+
+
+kilometer2 = Convert.ToDouble(Console.ReadLine());
+
+double mile2;
+mile2 = kilometer2 / 1.60934;
+Console.WriteLine("Miles= " + mile2);
---
+++ lenght converter/lenght converter/Program.cs
@@ -0,0 +1,34 @@
+﻿//Write a program that asks the user for an input value and then also asks if the value is a miles or kilometer value.
+//If the value is in miles, convert it to kilometers and print it, and vice versa for kilometers.
+
+double number;
+string type;
+
+Console.WriteLine("Length converter");
+Console.WriteLine("Please input a number value:");
+
+number = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What kind of value is this? (kilometers/miles)");
+
+type= Console.ReadLine();
+type = type.ToLower();
+
+if(type=="kilometers" || "kilometers".StartsWith(type) || type=="km" || type=="kms")
+{
+    double mile;
+
+    mile = number / 1.60934;
+    Console.WriteLine("Miles= " + mile);
+}
+else if(type=="miles" || "miles".StartsWith(type))
+{
+    double km;
+
+    km = number * 1.60934;
+    Console.WriteLine("Kilometers= " + km);
+}
+else
+{
+    Console.WriteLine("Sorry, my mom didn't tell me how to convert that, please use kilometers or miles");
+}
